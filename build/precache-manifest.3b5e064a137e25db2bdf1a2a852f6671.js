ab3d47b43f7b89e7ecc7da1015852f31d6ea8d71
Andrés <af.olivares10@uniandes.edu.co>
2020-01-30 23:16:44 -0500

    Initial commit

---
+++ build/precache-manifest.3b5e064a137e25db2bdf1a2a852f6671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "186165f5cdc7b941c142",
+    "url": "/static/js/main.186165f5.chunk.js"
+  },
+  {
+    "revision": "03f07d09225f19b4d54a",
+    "url": "/static/js/1.03f07d09.chunk.js"
+  },
+  {
+    "revision": "186165f5cdc7b941c142",
+    "url": "/static/css/main.bc5ef5f8.chunk.css"
+  },
+  {
+    "revision": "03f07d09225f19b4d54a",
+    "url": "/static/css/1.c0b54a48.chunk.css"
+  },
+  {
+    "revision": "97d533f1f272e080d9c0454627ab42f8",
+    "url": "/index.html"
+  }
+];
